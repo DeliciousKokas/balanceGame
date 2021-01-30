65500f5fba942441a44d3a6b8109cafe06df45e3
Jun <pajums0235@gmail.com>
2021-01-30 23:40:56 +0900

    Add counter vanilla js

--- conter.js
+++ conter.js
@@ -1,13 +1,45 @@
 const counter = document.querySelector(".js-counter")
+  countBtn = document.querySelector(".js-count")
+  resetBtn = document.querySelector(".js-reset")
 
-let count = 0
+let count = localStorage.getItem("count")
 
-function clickCounter(){
-  return 1
+function paintCounter(){
+  if (count === null) {count = 0}
+  if (count < 2) {
+    countBtn.innerHTML = "It has been clicked " + count+ "time"
+  } else {
+    countBtn.innerHTML = "It has been clicked " + count+ "times"
+  }
+}
+
+function countSubmit(){
+  count = parseInt(count) + 1
+  localStorage.setItem("count", count)
+  paintCounter()
+}
+
+function handleSubmit(){
+  countBtn.addEventListener("click", countSubmit)
+}
+
+function resetCounter(){
+  resetConfirm = window.confirm("Are you sure?")
+  if (resetConfirm) {
+    count = 0
+    localStorage.setItem("count", count)
+    paintCounter()
+  }
+}
+
+function handleReset(){
+  resetBtn.addEventListener("click", resetCounter)
 }
 
 function init(){
-  counter.innerHTML = "It has been clicked " + clickCounter() + "times"
+  paintCounter()
+  handleSubmit()
+  handleReset()
 }
 
 init()

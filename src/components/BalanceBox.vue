a8c4bef18ff00344f74553bff2f7ea0e310bebce
Jun <pajums0235@gmail.com>
2021-05-05 14:40:25 +0900

    Chage overoll css

--- src/components/BalanceBox.vue
+++ src/components/BalanceBox.vue
@@ -1,14 +1,27 @@
 <template>
   {{ card.title }}
   <div class="balanceBox">
-    <div class="balanceBox__box--left">
-      {{ card.left }}
+    <div class="balanceBox__left">
+      <div class="balanceBox__left--icon">
+        <font-awesome-icon
+          class="interFace__next--click"
+          :icon="['fas', 'dog']" 
+        />
+      </div>
+      <div class="balanceBox__left--text">
+        <span>{{ card.left }}</span>
+      </div>
     </div>
-    <div class="balanceBox__center">
-      <span>VS</span>
-    </div>
-    <div class="balanceBox__box--right">
-      {{ card.right }}
+    <div class="balanceBox__right">
+      <div class="balanceBox__right--icon">
+        <font-awesome-icon
+          class="interFace__next--click"
+          :icon="['fas', 'cat']" 
+        />
+      </div>
+      <div class="balanceBox__right--text">
+        <span>{{ card.right }}</span>
+      </div>
     </div>
   </div>
   <div class="interFace">
@@ -16,25 +29,24 @@
       class="interFace__like"
       @click="postLike()"
     >
-      <transition name="postLike" mode="out-in">
-        <font-awesome-icon
-          v-if=!card.liked
-          class="interFace__like--false"
-          :icon="['far', 'thumbs-up']" 
-        />
-        <font-awesome-icon
-          v-else
-          class="interFace__like--true"
-          :icon="['fas', 'check']" 
-        />
-      </transition>
+      <font-awesome-icon
+        v-if=!card.liked
+        class="interFace__like--false"
+        :icon="['far', 'thumbs-up']" 
+      />
+      <font-awesome-icon
+        v-else
+        class="interFace__like--true"
+        :icon="['fas', 'check']" 
+      />
     </div>
     <div
       class="interFace__next"
       @click="getCard()"
     >
-      <span>Next</span> 
+      <span>Next</span>
       <font-awesome-icon
+        class="interFace__next--click"
         :icon="['fas', 'forward']" 
       />
     </div>
@@ -62,39 +74,64 @@ export default defineComponent({
   width: 100%;
 
   %__box {
+    display: flex;
+    flex-direction: column;
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     font-size: 16px;
     font-weight: bold;
     width: 100%;
-    height: 150px;
-    color: white;
-    padding: 10px;
-    margin: 5px;
+    height: 200px;
+    margin: 10px;
+    border-radius: 5px;
+    box-shadow: 0px 3px 4px rgba(0,0,0,0.3);
     cursor: pointer;
     
     &:hover{
       transform: scale(1.02);
       transition: transform .2s;
+    } 
+
+    &:active{
+      transform: scale(1);
     }
   }
 
-  &__box{
+  %--icon{
+    color: rgb(255, 255, 255);
+    border-radius: 5px 5px 0 0;
+    padding: 5px;
+  }
 
-    &--left{
-      @extend %__box;
+  %--text{
+    border-radius: 0 0 5px 5px;
+    padding: 15px;
+    margin: auto;
+  }
+
+  &__left{
+    @extend %__box;
+
+    &--icon{
+      @extend %--icon;
       background-color: rgb(255, 0, 0);
     }
 
-    &--right{
-      @extend %__box;
-      background-color: rgb(0, 0, 255);
+    &--text{
+      @extend %--text;
     }
   }
 
-  &__center{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+  &__right{
+    @extend %__box;
+    
+    &--icon{
+      @extend %--icon;
+      background-color:rgb(0, 0, 255);
+    }
+
+    &--text{
+      @extend %--text;
+    }
   }
 
 }
@@ -104,14 +141,6 @@ export default defineComponent({
   justify-content: flex-end;
   align-items: center;
 
-  .postLike-enter-active, .postLike-leave-active {
-    transition: opacity .4s
-  }
-
-  .postLike-enter, .postLike-leave-to {
-    opacity: 0.05
-  }
-
   %__like {
     border-color: rgb(0, 0, 255);
     border: 1px;
@@ -119,12 +148,17 @@ export default defineComponent({
     border-style: solid;
     padding: 10px;
     display: inline-block;
-    box-shadow: 0 2px 4px rgba(0,0,0,0.3);
+    box-shadow: 0 3px 4px rgba(0,0,0,0.3);
+    cursor: pointer;
   
     &:hover{
-      transform: scale(1.1);
-      // transition: transform .2s;
+      transform: scale(1.12);
+      transition: transform .2s;
     }
+
+    &:active{
+      transform: scale(1);
+    } 
   }
 
   &__like{
@@ -147,20 +181,23 @@ export default defineComponent({
   &__next{
     display: flex;
     align-items: center;
-
     border: 1px;
-    border-radius: 5%;
+    border-radius: 3px;
     border-style: solid;
     padding: 3px 10px;
-    margin: 5px;
+    margin: 10px;
     cursor: pointer;
-    box-shadow: 0 2px 4px rgba(0,0,0,0.3);
+    box-shadow: 0px 3px 4px rgba(0,0,0,0.3);
 
     &:hover{
-      transform: scale(1.05);
+      transform: scale(1.08);
       transition: transform .2s;
     }
-    
+
+    &:active{
+      transform: scale(1);
+    } 
+
     span{
       font-weight: bold;
       margin: 5px;
